24d1d100c40e2debf68283cc98f260e7925cbe64
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-02 14:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from diffusers import FluxPipeline
-import torch
 from io import BytesIO
 import base64
 
 class InferlessPythonModel:
     def initialize(self):
-        self.pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16).to("cuda")
+        self.pipe = "I am a model pipeline"
 
     def infer(self, inputs):
         prompt = inputs["prompt"]
@@ -15,20 +13,7 @@ class InferlessPythonModel:
         num_inference_steps = inputs.get("num_inference_steps", 4)
         max_sequence_length = inputs.get("max_sequence_length", 256)
 
-        image = self.pipe(
-            prompt,
-            height=height,
-            width=width,
-            guidance_scale=guidance_scale,
-            num_inference_steps=num_inference_steps,
-            max_sequence_length=max_sequence_length,
-        ).images[0]
-
-        buff = BytesIO()
-        image.save(buff, format="JPEG")
-        img_str = base64.b64encode(buff.getvalue()).decode()
-
-        return {"generated_image_base64": img_str}
+        return {"generated_image_base64": "this is a base64 image str"}
 
     def finalize(self):
         self.pipe = None
